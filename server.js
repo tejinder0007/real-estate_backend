881179a617b04b1ed06bf5e4dc81ed446c5e725a
Tejinder Singh <singhslatch716@gmail.com>
2026-01-07 23:11:24 +0530

    edit error

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 
 // --- Middleware ---
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://https://real-estate-frontend-liard-rho.vercel.app"], // Add your frontend URLs here
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // --- Define API Routes ---
